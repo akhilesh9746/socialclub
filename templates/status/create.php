ad7e0bdf091fd1bd136e0b6644a245454d989c02
bps7j <bps7j>
2005-06-05 18:16:21 +0000

    removed

--- templates/status/create.php
+++
@@ -1,8 +0,0 @@
-<h1>Create a Status</h1>
-
-<p>Use this page to create a new status.  You are adding to a fundamentally
-important part of the website's administrative interface; don't do this unless
-you understand what a status is, how it interacts with privileges, and so
-forth.</p>
-
-{FORM}
--- templates/status/list_all.php
+++
@@ -1,7 +0,0 @@
-<h1>Status Objects</h1>
-
-<p>The following is a list of all status objects in the database:</p>
-
-<ol>{ITEM:}
-  <li><a href="members/status/read/{C_UID}"><b>{C_TITLE}</b></a> {C_DESCRIPTION}</a></li>{:ITEM}
-</ol>
