1e439fa4c6ea25a227c53f57c885a35dab45163d
bps7j <bps7j>
2005-06-05 16:21:23 +0000

    move lots of code between various include files

--- includes/setup.php
+++ includes/setup.php
@@ -17,7 +17,7 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307  USA
  * 
- * $Id: setup.php,v 1.1 2005/03/27 19:54:21 bps7j Exp $
+ * $Id: setup.php,v 1.2 2005/06/05 16:21:23 bps7j Exp $
  *
  * Create the variables and stuff the individual pages need, including
  * setting up error handling and global variables.
@@ -25,6 +25,15 @@
 
 # Include some global functions
 require_once("functions.php");
+
+# Require various classes
+include_once("table.php");
+include_once("Template.php");
+include_once("XmlForm.php");
+
+# You need to include this file after the ones above or PHP will throw an
+# "undefined class database_object" from privilege.php
+include_once("member.php");
 
 # -----------------------------------------------------------------------------
 # Miscellaneous stuff
@@ -35,11 +44,11 @@ ignore_user_abort(true);
 srand ((double) microtime() * 1000000);
 
 # -----------------------------------------------------------------------------
-# There are ONLY FOUR global variables,
-# $cfg $res, $err, and $obj.  $cfg holds config information, $res holds generated
-# content to send to the browser, and $obj holds global objects.  See the end of
-# this file for definitions of what's expected to be in the $res variable.  $err
-# holds a list of errors that can be spit out for debugging.
+# Global variables.  There are ONLY FOUR global variables, $cfg $res, $err, and
+# $obj.  $cfg holds config information, $res holds generated content to send to
+# the browser, and $obj holds global objects.  See below for definitions of
+# what's expected to be in the $res variable.  $err holds a list of errors that
+# can be spit out for debugging.
 # -----------------------------------------------------------------------------
 
 $cfg = array();
@@ -86,7 +95,7 @@ function userErrorHandler($errno, $errstr, $errfile, $errline) {
         error_log($logMessage, 1, $cfg['webmaster_email']);
     }
     if ((intval($errno) & ERROR_LOGGING) != 0) {
-        error_log(date("Y-m-d h:i:s", time()) . $logMessage . "\r\n", 3, $cfg['error_log']);
+        error_log(date("Y-m-d h:i:s ", time()) . $logMessage . "\r\n", 3, $cfg['error_log']);
     }
 }
 set_error_handler("userErrorHandler");
@@ -181,17 +190,26 @@ function fatalErrorHandler(&$buffer) {
     }
 }
 
+# ------------------------------------------------------------------------------
+# Create the database connection.
+# ------------------------------------------------------------------------------
+include_once("{$cfg['db']['type']}.php");
+$obj['conn'] =& new $cfg['db']['type']($cfg['db']);
+$obj['conn']->open();
+
+# ------------------------------------------------------------------------------
 # Login mode.  See the comments in the header of includes/authorize.php.  Pages
 # that want to require someone to log in should include authorize.php.  The
 # login status indicates success or failure of the login attempt.  The
 # login_password indicates if the user entered the right password.  The
 # login_exists indicates if the email address even exists in the database.
+# ------------------------------------------------------------------------------
 $cfg['login_mode'] = "full";
 $cfg['login_status'] = true;
 $cfg['login_password'] = true;
 $cfg['login_exists'] = true;
 
-# The user and password
+# The userid and password cookie, for logging in the user.
 if (isset($_COOKIE['auth'])) {
     $cfg['auth'] = unserialize(base64_decode($_COOKIE['auth']));
 }
@@ -199,56 +217,12 @@ else {
     $cfg['auth'] = array("user" => "", "pass" => "");
 }
 
+# ------------------------------------------------------------------------------
 # Holds a list of references to objects that have been 'visited' before.  This
 # is to avoid cycling when deciding which objects to delete (when cascading
 # deletes).
+# ------------------------------------------------------------------------------
 $obj['visited-objects'] = array();
-
-# -----------------------------------------------------------------------------
-# Arrays of data that are initialized from the database.  These arrays could be
-# hard coded into the pages, but for flexibility it is better to retrieve them
-# from the database.  They enable you to refer to an action, status or group by
-# title instead of number.  Use them like $cfg['group_id']['root']
-# -----------------------------------------------------------------------------
-
-# The status codes, created from [_]status.
-$cfg['status_id'] = array();
-
-# Actions.  The codes are in the form title => uid, and the labels are in the
-# form uid => "human-readable-description".  The flipped codes are in reverse
-# order so you can look up the title from the uid.  Created from [_]action.
-$cfg['action_id'] = array();
-$cfg['action_summary'] = array();
-$cfg['action_title'] = array();
-
-# This is a list of actions that simply can't be done without an object.  It is
-# initialized from the c_flags field on [_]action in the database.
-$cfg['require_object_actions'] = array();
-
-# Usergroups, created from [_]group.
-$cfg['group_id'] = array();
-
-# Flags, set in an object's c_flags value.  Use getFlag($flagName) and
-# setFlag($flagName, bool) on any DatabaseObject class.  Populated from [_]flags.
-$cfg['flag'] = array();
-
-# Unix permission bitmasks, set in an object's c_unixperms and used the same as
-# flags.  Populated from [_]unixperms.
-$cfg['perm'] = array();
-
-# Tables in the database.  There are actually a few other tables that are "meta"
-# tables; these are the ones that hold information the PHP can manipulate in a
-# uniform way.
-$cfg['tables'] = array();
-
-# Require various classes
-include_once("table.php");
-include_once("Template.php");
-include_once("XmlForm.php");
-
-# You need to include this file after the ones above or PHP will throw an
-# "undefined class DatabaseObject" from Privilege.php
-include_once("member.php");
 
 # ------------------------------------------------------------------------------
 # The following indices are defined in the $res array:
@@ -261,6 +235,7 @@ include_once("member.php");
 # keywords      The keywords META tag
 # navbar        The navbar contents
 # tabs          The tabbed-box tabs HTML
+# help          The WikiName of the help topic (should begin with 'HelpOn').
 # ------------------------------------------------------------------------------
 $res['keywords'] = "";
 $res['description'] = "";
@@ -268,59 +243,107 @@ $res['title'] = "";
 $res['navbar'] = "Home";
 $res['tabs'] = "";
 $res['meta'] = "";
+$res['help'] = "";
 
 # ------------------------------------------------------------------------------
-# Create global objects
+# ------------------------------------------------------------------------------
+# Definitions of constants that are required to run the website.
+# ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-# Create the database connection
-include_once("{$cfg['db']['type']}.php");
-$obj['conn'] =& new $cfg['db']['type']($cfg['db']);
-$obj['conn']->open();
+# ------------------------------------------------------------------------------
+# Flags, set in an object's c_flags value.  Use getFlag($flagName) and
+# setFlag($flagName, bool) on any DatabaseObject class.
+# ------------------------------------------------------------------------------
+$cfg['flag'] = array(
+    "private" => 1,
+    "email_private" => 2,
+    "unused_1" => 4,
+    "student" => 8,
+    "flexible" => 16,
+    "has_photo" => 32,
+    "receive_email" => 64,
+    "reimbursable" => 128,
+    "applies_to_object" => 256,
+    "primary" => 512,
+    "unused_2" => 1024,
+    "generic" => 2048
+    );
 
-# ----------------------------------------------------------------------------
-# Perform any Magical Initialization that needs to be done.
-# ----------------------------------------------------------------------------
-# Set up the flags
-$result =& $obj['conn']->query("select c_title, c_bitmask from [_]flag");
-while ($row =& $result->fetchRow()) {
-    $cfg['flag'][$row['c_title']] = intval($row['c_bitmask']);
-}
+# ------------------------------------------------------------------------------
+# User Groups
+# ------------------------------------------------------------------------------
+$cfg['group_id'] = array(
+    "root" => 1,
+    "officer" => 2,
+    "treasurer" => 4,
+    "leader" => 8,
+    "quartermaster" => 16,
+    "member" => 32,
+    "guest" => 64,
+    "wheel" => 128,
+    "activator" => 256
+    );
 
-# Set up the permission flags
-$result =& $obj['conn']->query("select c_title, c_bitmask from [_]unixperm");
-while ($row =& $result->fetchRow()) {
-    $cfg['perm'][$row['c_title']] = intval($row['c_bitmask']);
-}
+# ------------------------------------------------------------------------------
+# Unix permission bitmasks, set in an object's c_unixperms and used the same as
+# flags.  These are the rightmost 9 bits, as you would see in UNIX files.
+# ------------------------------------------------------------------------------
+$cfg['perm'] = array(
+    "owner_read" => 256,
+    "owner_write" => 128,
+    "owner_delete" => 64,
+    "group_read" => 32,
+    "group_write" => 16,
+    "group_delete" => 8,
+    "other_read" => 4,
+    "other_write" => 2,
+    "other_delete" => 1
+    );
 
-# Set up the status codes
-$result =& $obj['conn']->query("select c_title, c_uid from [_]status order by c_uid");
-while ($row =& $result->fetchRow()) {
-    $cfg['status_id'][$row['c_title']] = $row['c_uid'];
-}
+# ------------------------------------------------------------------------------
+# Status values.  You can set statuses on any database_object with the xxxStatus
+# functions. TODO: implement more xxxStatus functions.
+# ------------------------------------------------------------------------------
+$cfg['status_id'] = array(
+    "default" => 1,
+    "deleted" => 2,
+    "inactive" => 4,
+    "active" => 8,
+    "waitlisted" => 16,
+    "cancelled" => 32,
+    "pending" => 64,
+    "paid" => 128,
+    "checked_out" => 256,
+    "checked_in" => 512,
+    "missing" => 1024,
+    "submitted" => 2048
+    );
 
-# Set up the action codes and labels, and list of actions that require
-# an object
-$result =& $obj['conn']->query("select c_title, c_uid, c_flags, "
-    . "c_summary from [_]action order by c_uid");
+# ------------------------------------------------------------------------------
+# Actions.
+# ------------------------------------------------------------------------------
+$cfg['actions'] = array();
+
+# This is a list of actions that simply can't be done without an object.  It is
+# initialized from the c_flags field on [_]action in the database.
+$cfg['require_object_actions'] = array();
+
+# Set up the actions.
+$result =& $obj['conn']->query("select * from [_]action order by c_title");
 while ($row =& $result->fetchRow()) {
-    $cfg['action_id'][$row['c_title']] = $row['c_uid'];
-    $cfg['action_summary'][$row['c_uid']] = $row['c_summary'];
+    $cfg['actions'][$row['c_title']] = $row;
     if (intval($row['c_flags']) & $cfg['flag']['applies_to_object']) {
-        $cfg['require_object_actions'][] = $row['c_uid'];
+        $cfg['require_object_actions'][] = $row['c_title'];
     }
 }
 
-# Set up the flipped action codes
-$cfg['action_title'] = array_flip($cfg['action_id']);
-
-# Set up the group codes
-$result =& $obj['conn']->query("select c_title, c_uid from [_]group order by c_uid");
-while ($row =& $result->fetchRow()) {
-    $cfg['group_id'][$row['c_title']] = $row['c_uid'];
-}
-
-# Set up tables
+# ------------------------------------------------------------------------------
+# Tables in the database.  There are actually a few other tables that are "meta"
+# tables; these are the ones that hold information the site can manipulate in a
+# uniform way.
+# ------------------------------------------------------------------------------
+$cfg['tables'] = array();
 $result =& $obj['conn']->query("select c_name from [_]table order by c_name");
 while ($row =& $result->fetchRow()) {
     $cfg['tables'][] = $row['c_name'];
