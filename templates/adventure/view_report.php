4b6bf73268bf88c1767a9b372fd5266a89b3899d
bps7j <bps7j>
2005-06-05 18:11:25 +0000

    add JavaScript validation to form

--- templates/adventure/view_report.php
+++ templates/adventure/view_report.php
@@ -17,7 +17,35 @@ absences and times the member has been waitlisted.  Use the radio button in the
 left column, and the buttons at the bottom of the table, to manage
 attendees.</p>
 
-<form action="members.php" method="GET">
+<script type="text/javascript" language="javascript">
+function formEnabled(form) {
+    var left = false;
+    var bottom = false;
+    if (form.elements["object"].checked) {
+        left = true;
+    }
+    else {
+        for (var i = 0; i < form.elements["object"].length; ++i) {
+            alert (form.elements["object"][i].checked);
+            if (form.elements["object"][i].checked) {
+                left = true;
+            }
+        }
+    }
+    for (var i = 0; i < form.elements["action"].length; ++i) {
+        if (form.elements["action"][i].checked) {
+            bottom = true;
+        }
+    }
+    return (left && bottom);
+}
+
+function enableForm(form) {
+    form.elements['submit'].disabled = !formEnabled(form);
+}
+</script>
+
+<form action="index.php" method="GET" onSubmit="return formEnabled(this)">
   <input type="hidden" name="page" value="attendee">
 <table class="compact cleanHeaders top">
   <tr>
@@ -32,7 +60,7 @@ attendees.</p>
   </tr>{ROW:}
   <tr>
     <td>
-      <input type="radio" class="compact" name="object" value="{C_UID}">
+      <input type="radio" class="compact" name="object" value="{C_UID}" onClick="enableForm(this.form)">
     </td>
     <td>{C_STATUS}</td>
     <td>
@@ -50,15 +78,15 @@ attendees.</p>
   </tr>
 {:ROW}
   <tr><td colspan="8" style="border-top:1px solid #000; text-align:right">
-  <input type="radio" name="action" value="{ACTION_WITHDRAW}" id="action1">
+  <input type="radio" name="action" value="withdraw" id="action1" onClick="enableForm(this.form)">
   <label for="action1">Withdraw</label>
-  <input type="radio" name="action" value="{ACTION_WAITLIST}" id="action2">
+  <input type="radio" name="action" value="waitlist" id="action2" onClick="enableForm(this.form)">
   <label for="action2">Waitlist</label>
-  <input type="radio" name="action" value="{ACTION_JOIN}" id="action3">
+  <input type="radio" name="action" value="join" id="action3" onClick="enableForm(this.form)">
   <label for="action3">Join from Waitlist</label>
-  <input type="radio" name="action" value="{ACTION_MARK_ABSENT}" id="action4">
+  <input type="radio" name="action" value="mark_absent" id="action4" onClick="enableForm(this.form)">
   <label for="action4">Mark Absent</label>
-  <input type="submit" value="Go">
+  <input type="submit" name="submit" value="Go" disabled="true">
   </td></tr>
 </table>
 </form>
@@ -89,9 +117,9 @@ attendees.</p>
 <p class="notice">There is nothing to display.</p>
 {:NONE}
 
-<p><sup>[1]</sup>After a member is automatically removed from the waitlist
-his/her joined date is reset, so s/he appears at the end of the roster.  This is
-correct behavior.  It doesn't mean that people who joined later are getting off
-the waitlist first.</p>
+<p><sup>[1]</sup><span class="tiny">After a member is automatically removed from
+the waitlist his/her joined date is reset, so s/he appears at the end of the
+roster.  This is correct behavior.  It doesn't mean that people who joined later
+are getting off the waitlist first.</span></p>
 
 </div>
