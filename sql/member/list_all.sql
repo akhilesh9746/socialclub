7147b784c47b8187ddcd19d8ec846d3ab20da288
bps7j <bps7j>
2005-08-02 03:06:16 +0000

    add support for paging, add parameters for statuses, remove references to c_flags

--- sql/member/list_all.sql
+++ sql/member/list_all.sql
@@ -6,23 +6,15 @@
  * hide_info int
  *      1 if the member is not supposed to see information the other members
  *      have marked as private
- * email_private int
- *      The bitmask for the preference to keep email private
- * private int
- *      The bitmask for the preference for all info to be private
- * primary int
- *      The bitmask for the flag that says this item is primary
  */
 select
     me.c_uid, 
     me.c_first_name, 
     me.c_last_name,
-    case when ({hide_info,int} and (me.c_flags & {email_private,int}))
+    case when ({hide_info,int} and me.c_email_hidden)
         then '[private]'
         else me.c_email end as c_email,
-    case when (
-        ph.c_uid is null 
-        or ({hide_info,int} and (ph.c_flags & {private,int})))
+    case when (ph.c_uid is null or ({hide_info,int} and ph.c_hidden))
         then '[private]'
         else concat(
             '(', ph.c_area_code, ') ', 
@@ -30,7 +22,7 @@ select
         end as phone_number,
     coalesce(ct.c_abbreviation, '') as c_abbreviation,
     case when (ch.c_uid is null) then ""
-        when ({hide_info,int} and (ch.c_flags & {private,int})) then ""
+        when ({hide_info,int} and ch.c_hidden) then ""
         else ch.c_screenname
         end as c_screenname
 from 
@@ -38,11 +30,11 @@ from
     inner join [_]membership as ms on ms.c_member = me.c_uid
     left outer join [_]phone_number as ph 
         on ph.c_owner = me.c_uid
-        and (ph.c_flags & {primary,int})
+        and ph.c_primary = 1
         and ph.c_deleted <> 1
     left outer join [_]chat as ch
         on ch.c_owner = me.c_uid
-        and (ch.c_flags & {primary,int})
+        and ch.c_hidden = 1
         and ch.c_deleted <> 1
     left outer join [_]chat_type as ct on ct.c_uid = ch.c_type
         and ct.c_deleted <> 1
@@ -52,12 +44,11 @@ where
         and now() >= ms.c_begin_date 
         and now() <= ms.c_expiration_date
     ))
-    and ({view_private,int} > 0 or (
-        me.c_flags & {private,int} = 0
-    ))
+    and ({view_private,int} > 0 or me.c_hidden = 0)
     and ms.c_deleted <> 1
     and me.c_deleted <> 1
     and ({name,char} is null or me.c_full_name like {name,char})
     and ({email,char} is null or me.c_email like {email,char})
 group by me.c_uid
 order by {orderby,none,,,0,me.c_last_name}
+limit {offset,int,,,0,0},{limit,int}
