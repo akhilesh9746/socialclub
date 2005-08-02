10e6530a0af6b26b264a3855e84ae8ad38786778
bps7j <bps7j>
2005-08-02 03:02:49 +0000

    do not hardcode status values

--- schema/implemented-actions.sql
+++ schema/implemented-actions.sql
@@ -16,11 +16,17 @@
  * this program.  If not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307  USA
  *
- * $Id: implemented-actions.sql,v 1.2 2005/06/05 18:07:47 bps7j Exp $
+ * $Id: implemented-actions.sql,v 1.3 2005/08/02 03:02:49 bps7j Exp $
  *
  * Create correspondences between tables and actions to say which actions apply
  * to which tables.
  */
+
+-- Declare status value constants.
+set @ns_default = 1, @ns_inactive = 2, @ns_active = 4,
+    @ns_waitlisted = 8, @ns_cancelled = 16, @ns_pending = 32, @ns_paid = 64,
+    @ns_checked_out = 128, @ns_checked_in = 256, @ns_missing = 512,
+    @ns_submitted = 1024;
 
 delete from [_]implemented_action;
 
@@ -30,43 +36,43 @@ insert into [_]implemented_action (c_table, c_action)
     from [_]action, [_]table
     where c_title in ("read", "write", "delete", "list_all", "list_owned_by",
         "create", "stat", "chmod", "chgrp", "chown", "chmeta", "view_acl",
-        "set_flags", "copy", "add_privilege");
+        "copy", "add_privilege");
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values 
-    ("[_]checkout", "accept", 1),
-    ("[_]checkout", "check_in", 256);
+    ("[_]checkout", "accept", @ns_default),
+    ("[_]checkout", "check_in", @ns_checked_out);
 -- Only allow editing when it is in default status.
-update [_]implemented_action set c_status = 1
+update [_]implemented_action set c_status = @ns_default
 where c_table = "[_]checkout" and c_action = "write";
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]expense_submission", "accept", 2048),
-    ("[_]expense_submission", "submit", 1);
+    ("[_]expense_submission", "accept", @ns_submitted),
+    ("[_]expense_submission", "submit", @ns_default);
 -- Only allow editing when it is in default status.
-update [_]implemented_action set c_status = 1
+update [_]implemented_action set c_status = @ns_default
 where c_table = "[_]expense_submission" and c_action = "write";
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]adventure", "join", 9),
-    ("[_]adventure", "withdraw", 8),
+    ("[_]adventure", "join", @ns_default | @ns_active),
+    ("[_]adventure", "withdraw", @ns_active),
     ("[_]adventure", "edit_questions", 0),
     ("[_]adventure", "choose_activities", 0),
     ("[_]adventure", "view_report", 0),
-    ("[_]adventure", "announce", 8),
-    ("[_]adventure", "activate", 5),
-    ("[_]adventure", "email_attendees", 8),
-    ("[_]adventure", "comment", 8),
-    ("[_]adventure", "view_waitlist", 8);
+    ("[_]adventure", "announce", @ns_active),
+    ("[_]adventure", "activate", @ns_default | @ns_inactive | @ns_cancelled),
+    ("[_]adventure", "email_attendees", @ns_default),
+    ("[_]adventure", "comment", @ns_default),
+    ("[_]adventure", "view_waitlist", @ns_default);
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]attendee", "withdraw", 1),
-    ("[_]attendee", "join", 16),
-    ("[_]attendee", "waitlist", 1),
-    ("[_]attendee", "mark_absent", 1),
+    ("[_]attendee", "withdraw", @ns_default | @ns_active | @ns_waitlisted),
+    ("[_]attendee", "join", @ns_waitlisted),
+    ("[_]attendee", "waitlist", @ns_active | @ns_default),
+    ("[_]attendee", "mark_absent", @ns_active | @ns_default),
     ("[_]attendee", "view_answers", 0);
 
 insert into [_]implemented_action (c_table, c_action)
@@ -83,7 +89,7 @@ insert into [_]implemented_action (c_table, c_action)
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]membership", "activate", 4);
+    ("[_]membership", "activate", @ns_inactive);
 
 insert into [_]implemented_action (c_table, c_action)
     select "[_]member", c_title from [_]action
@@ -105,10 +111,10 @@ insert into [_]implemented_action (c_table, c_action)
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]classified_ad", "deactivate", 9);
+    ("[_]classified_ad", "deactivate", @ns_default | @ns_active);
 
 insert into [_]implemented_action (c_table, c_action, c_status)
 values
-    ("[_]expense_report", "submit", 1),
-    ("[_]expense_report", "accept", 64),
+    ("[_]expense_report", "submit", @ns_default),
+    ("[_]expense_report", "accept", @ns_pending),
     ("[_]expense_report", "view_notes", 0);
