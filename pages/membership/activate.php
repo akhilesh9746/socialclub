c258153d118181e0616284c2df7d57a57b77e1dc
bps7j <bps7j>
2005-08-02 02:59:13 +0000

    lowercase template keys, stop using c_flags, do not assign by reference, skip a 'sanity check' that never would be violated

--- pages/membership/activate.php
+++ pages/membership/activate.php
@@ -17,7 +17,7 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307  USA
  * 
- * $Id: activate.php,v 1.1 2005/03/27 19:53:14 bps7j Exp $
+ * $Id: activate.php,v 1.2 2005/08/02 02:59:13 bps7j Exp $
  */
 
 require_once("membership_type.php");
@@ -33,122 +33,116 @@ $type->select($object->getType());
 $member =& new member();
 $member->select($object->getMember());
 
-# Sanity check: must be inactive
-if ($object->getStatus() == $cfg['status_id']['inactive']) {
+if ($type->getFlexible()) {
+    $template = Template::unhide($template, "FLEX");
+}
+else {
+    $template = Template::unhide($template, "INFLEX");
+}
 
-    if ($type->getFlag("flexible")) {
-        $template = Template::unhide($template, "FLEX");
-    }
-    else {
-        $template = Template::unhide($template, "INFLEX");
-    }
+# Determine the age of the member
+$now = getdate();
+$born = getdate(strtotime($member->getBirthDate()));
+$months = ($now['mon'] - $born['mon'] + 12) % 12;
+$years = $now['year'] - $born['year'];
+if ($now['mon'] < $born['mon']) {
+    $years -= 1;
+}
+$template = Template::replace($template, array(
+    "AGE" => "$years years and $months months")); 
 
-    # Determine the age of the member
-    $now = getdate();
-    $born = getdate(strtotime($member->getBirthDate()));
-    $months = ($now['mon'] - $born['mon'] + 12) % 12;
-    $years = $now['year'] - $born['year'];
-    if ($now['mon'] < $born['mon']) {
-        $years -= 1;
-    }
-    $template = Template::replace($template, array(
-        "AGE" => "$years years and $months months")); 
+# Get the max expiration date of the user's active memberships from the
+# database
+$cmd = $obj['conn']->createCommand();
+$cmd->loadQuery("sql/membership/select-max-expiration-date.sql");
+$cmd->addParameter("active", $cfg['status_id']['active']);
+$cmd->addParameter("member", $object->getMember());
+$maxExpire =& new DateTime($cmd->executeScalar());
 
-    # Get the max expiration date of the user's active memberships from the
-    # database
-    $cmd =& $obj['conn']->createCommand();
-    $cmd->loadQuery("sql/membership/select-max-expiration-date.sql");
-    $cmd->addParameter("member", $object->getMember());
-    $maxExpire =& new DateTime($cmd->executeScalar());
-
-    # Figure out when the suggested end date should be.  It depends on whether
-    # the membership type is flexible or not.
-    $begin =& new DateTime();
-    $expire =& new DateTime();
-    if ($type->getFlag("flexible")) {
-        $begin = $maxExpire;
-        switch ($type->getUnit()) {
-        case "year":
-            $expire = $begin->addYears($object->getUnitsGranted());
-            break;
-        case "month":
-            $expire = $begin->addMonths($object->getUnitsGranted());
-            break;
-        case "day":
-            $expire = $begin->addDays($object->getUnitsGranted());
-            break;
-        }
-    }
-    else {
-        # Just use the values in the membership itself; these were set when the
-        # membership was created.
-        $begin =& new DateTime($object->getBeginDate());
-        $expire =& new DateTime($object->getExpirationDate());
-    }
-
-    # Create the form and initialize it from the results of the date
-    # calculations above.
-    $form =& new XmlForm("forms/membership/activate.xml");
-    $form->setValue("begin", $begin->toString("Y-m-d"));
-    $form->setValue("expire", $expire->toString("Y-m-d"));
-    $form->setValue("totalcost", $object->getTotalCost());
-
-    # Overwrite the values in the form with what the user may have submitted
-    $form->snatch();
-    $form->validate();
-
-    if ($form->isValid()) {
-        $object->setStatus($cfg['status_id']['active']);
-        $object->setAmountPaid($form->getValue("totalcost"));
-        $object->setBeginDate($form->getValue("begin"));
-        $object->setExpirationDate($form->getValue("expire"));
-        $object->update();
-
-        # Add a note to the member to say who activated the membership
-        $member->addNote("$cfg[user] activated membership $cfg[object]");
-
-        $msg =& new Email();
-        $msgBody = file_get_contents("templates/emails/activation-notice.txt");
-        $msgBody = Template::replace($msgBody, array(
-            "CLUB_NAME" => $cfg['club_name'],
-            "TITLE" => $type->getTitle(),
-            "C_FULL_NAME" => $member->getFullName(),
-            "BASEURL" => $cfg['site_url'] . $cfg['base_url'])
-            + $object->getVarArray());
-        $msg->setBody($msgBody);
-        $msg->addTo($member->getEmail());
-        $msg->setFrom($cfg['club_admin_email_name']);
-        $msg->addHeader("Return-Path", $cfg['club_admin_email']);
-        $msg->loadFooter("templates/emails/footer.txt");
-        $msg->setSubject("Welcome to $cfg[club_name]!");
-        $msg->send();
-
-        # Record this transaction
-        $cmd =& $obj['conn']->createCommand();
-        $cmd->setCommandText("select c_uid from [_]expense_category "
-            . "where c_title = 'Membership Dues'");
-        $cat = $cmd->executeScalar();
-
-        # Record this transaction
-        $tran =& new transaction();
-        $tran->setAmount(floatval($form->getValue("totalcost")));
-        $tran->setCategory($cat);
-        $tran->setDescription($member->getFullName()
-            . "'s dues for membership $cfg[object]");
-        $tran->setFrom($cfg['object']);
-        $tran->setTo($cfg['root_uid']);
-        $tran->insert();
-
-        # Update the page
-        $template = Template::unhide($template, "SUCCESS");
-    }
-    else {
-        $template = Template::unhide($template, "INACTIVE");
-        $template = Template::replace($template, array("FORM" => $form->toString()));
+# Figure out when the suggested end date should be.  It depends on whether
+# the membership type is flexible or not.
+$begin =& new DateTime();
+$expire =& new DateTime();
+if ($type->getFlexible()) {
+    $begin = $maxExpire;
+    switch ($type->getUnit()) {
+    case "year":
+        $expire = $begin->addYears($object->getUnitsGranted());
+        break;
+    case "month":
+        $expire = $begin->addMonths($object->getUnitsGranted());
+        break;
+    case "day":
+        $expire = $begin->addDays($object->getUnitsGranted());
+        break;
     }
 }
 else {
-    $template = Template::unhide($template, "ACTIVE");
+    # Just use the values in the membership itself; these were set when the
+    # membership was created.
+    $begin =& new DateTime($object->getBeginDate());
+    $expire =& new DateTime($object->getExpirationDate());
+}
+
+# Create the form and initialize it from the results of the date
+# calculations above.
+$form =& new XmlForm("forms/membership/activate.xml");
+$form->setValue("begin", $begin->toString("Y-m-d"));
+$form->setValue("expire", $expire->toString("Y-m-d"));
+$form->setValue("totalcost", $object->getTotalCost());
+
+# Overwrite the values in the form with what the user may have submitted
+$form->snatch();
+$form->validate();
+
+if ($form->isValid()) {
+    $object->setStatus($cfg['status_id']['active']);
+    $object->setAmountPaid($form->getValue("totalcost"));
+    $object->setBeginDate($form->getValue("begin"));
+    $object->setExpirationDate($form->getValue("expire"));
+    $object->update();
+
+    # Add a note to the member to say who activated the membership
+    $member->addNote("$cfg[user] activated membership $cfg[object]");
+
+    $msg =& new Email();
+    $msgBody = file_get_contents("templates/emails/activation-notice.txt");
+    $msgBody = Template::replace($msgBody, array(
+        "CLUB_NAME" => $cfg['club_name'],
+        "TITLE" => $type->getTitle(),
+        "C_FULL_NAME" => $member->getFullName(),
+        "BASEURL" => $cfg['site_url'] . $cfg['base_url'])
+        + $object->getVarArray());
+    $msg->setBody($msgBody);
+    $msg->addTo($member->getEmail());
+    $msg->setFrom($cfg['club_admin_email_name']);
+    $msg->addHeader("Return-Path", $cfg['club_admin_email']);
+    $msg->loadFooter("templates/emails/footer.txt");
+    $msg->setSubject("Welcome to $cfg[club_name]!");
+    $msg->send();
+
+    # Record this transaction
+    $cmd = $obj['conn']->createCommand();
+    $cmd->setCommandText("select c_uid from [_]expense_category "
+        . "where c_title = 'Membership Dues'");
+    $cat = $cmd->executeScalar();
+
+    # Record this transaction
+    $tran =& new transaction();
+    $tran->setAmount(floatval($form->getValue("totalcost")));
+    $tran->setCategory($cat);
+    $tran->setDescription($member->getFullName()
+        . "'s dues for membership $cfg[object]");
+    $tran->setFrom($cfg['object']);
+    $tran->setTo($cfg['root_uid']);
+    $tran->insert();
+
+    # Update the page
+    $template = Template::unhide($template, "SUCCESS");
+}
+else {
+    $template = Template::unhide($template, "inactive");
+    $template = Template::replace($template, array("FORM" => $form->toString()));
 }
 
 $res['content'] = $member->insertIntoTemplate(
