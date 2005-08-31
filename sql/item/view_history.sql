5bcda309f8fafe5bb601ec1696c1c6082aaf7ed7
bps7j <bps7j>
2005-08-31 00:45:44 +0000

    return member info, and only get needed data

--- sql/item/view_history.sql
+++ sql/item/view_history.sql
@@ -1,4 +1,13 @@
-select *
-from [_]checkout_item
-where c_item = {item,int}
-    and c_deleted <> 1
+select
+    ci.c_checkout,
+    ci.c_created_date,
+    ci.c_status,
+    me.c_uid,
+    me.c_full_name
+from [_]checkout_item as ci
+    inner join [_]checkout as co on ci.c_checkout = co.c_uid
+    inner join [_]member as me on me.c_uid = co.c_member
+where ci.c_item = {item,int}
+    and ci.c_deleted = 0
+    and co.c_deleted = 0
+    and me.c_deleted = 0
