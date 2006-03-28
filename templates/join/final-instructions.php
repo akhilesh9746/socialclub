69c25e6eedd7d2194ed075b17494d393a066d6d4
bps7j <bps7j>
2006-03-28 03:13:22 +0000

    add links to liability waiver

--- templates/join/final-instructions.php
+++ templates/join/final-instructions.php
@@ -1,11 +1,23 @@
 <h1>Final Steps</h1>
 
-<p>Your inactive membership has been saved in the database.</p>
+<p>Your inactive membership has been saved in the database.  All you need to do
+at this point is print and sign the <a
+href="members/join/print-waiver">liability waiver</a>, and send it in with your
+dues.  <b>Please follow the instructions on the waiver</b>.</p>
+
+<p>You will need <a
+href="http://www.adobe.com/products/acrobat/readstep2.html">Adobe Reader</a> or
+similar software to view and print the liability waiver.  To print your
+liability waiver, <a href="members/join/print-waiver">click here</a>.</p>
+
+<p>We'll activate your membership and send you a welcome email as soon as we
+receive everything.</p>
 
 <h2>Amount Due</h2>
 
-<p>The following is a list of your inactive memberships saved in the database.
-If you see an old membership here, you can <a
+<p>Please send payment (instructions are on the waiver) for your membership.
+The following is a list of your inactive memberships saved in the database.  If
+you see an old membership here, you can <a
 href="members/join/delete-old">delete unwanted memberships</a>.</p>
 
 <table align="center" class="cleanHeaders" width="400">
