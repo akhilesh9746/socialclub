83c91d9979dc05b2069a1ee5de110a5b9ee74527
pctainto <pctainto>
2008-03-05 00:08:33 +0000

    Upgrading the query to use subqueries.  This drastically improves speed but does break compatibility with older mysql versions which couldn't handle subqueries

--- sql/item_type/select-with-available.sql
+++ sql/item_type/select-with-available.sql
@@ -3,24 +3,26 @@ select
     ic.c_title as cat_title,
     ty.c_uid,
     ty.c_title,
-    sum(case when (it.c_status = {missing,int}) then 0 else it.c_qty end)
-        - sum(coalesce(
-            case when (it.c_status & {checked_out,int}) then it.c_qty end,
-            cg.c_qty,
-            0
-        )) as available
+    it.existing
+        - sum(coalesce(it.it_out, 0) + coalesce(cg.g_out, 0)) as available
 from [_]item_category as ic 
     inner join [_]item_type as ty on ty.c_category = ic.c_uid
-    inner join [_]mutex as mu on mu.c_mutex in (0, 1)
-    left outer join [_]item as it on it.c_type = ty.c_uid
-        and mu.c_mutex = 0
-        and it.c_deleted = 0
-    left outer join [_]checkout_gear as cg on ty.c_uid = cg.c_type
-        and mu.c_mutex = 1
-        and cg.c_status = {checked_out,int}
-        and cg.c_deleted = 0
+    left outer join (
+        select it.c_type,
+            sum(if(it.c_status = {missing,int},0,it.c_qty)) as existing,
+            sum(if(it.c_status & {checked_out,int}, it.c_qty,0)) as it_out
+        from [_]item as it 
+        where it.c_deleted = 0
+        group by it.c_type
+    ) as it on it.c_type = ty.c_uid
+    left outer join (
+        select cg.c_type, sum(c_qty) as g_out 
+        from [_]checkout_gear as cg 
+        where cg.c_status = {checked_out,int} and cg.c_deleted = 0
+        group by cg.c_type
+    ) as cg on ty.c_uid = cg.c_type
 where ({cat,int} is null or ic.c_uid = {cat,int})
     and ty.c_deleted = 0
     and ic.c_deleted = 0
 group by ic.c_uid, ic.c_title, ty.c_uid, ty.c_title
-order by ic.c_title, ty.c_title
+order by ic.c_title, ty.c_title 
