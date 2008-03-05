42e377479a21ae0e1c1475e4d0cd30cbec816849
pctainto <pctainto>
2008-03-05 00:09:15 +0000

    fixing typo

--- sql/classified_ad/select-newest.sql
+++ sql/classified_ad/select-newest.sql
@@ -1,8 +1,9 @@
 select
     ad.c_uid,
-    ad.c_title
+    ad.c_title,
+    ad.c_created_date
 from [_]classified_ad as ad
-where (ad.c_status & {defaust,int} = {default,int})
+where (ad.c_status & {default,int} = {default,int})
     and ad.c_deleted <> 1
 order by ad.c_created_date desc
 limit {limit,int}
